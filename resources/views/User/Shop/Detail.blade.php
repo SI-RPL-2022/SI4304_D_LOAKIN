d8ae9222221073e49f672ae7cda1b331edee1f8d
amirajd <amirajauhara@gmail.com>
2022-05-16 22:58:27 +0700

    awal detail produk

---
+++ resources/views/User/Shop/Detail.blade.php
@@ -0,0 +1,82 @@
+<html>
+    <head>
+        <title>Detail Product</title>
+        <style>
+            @import url(https://fonts.googleapis.com/css?family=Raleway:400,300,500,700);
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background: #f5f4f3;
+  font-family: "Raleway";
+}
+body .card {
+  width: 750px;
+  height: 600px;
+  position: absolute;
+  background: white;
+  margin: 0 auto;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  transition: all 0.3s;
+  border-radius: 10px;
+}
+body .card .photo {
+  padding: 30px;
+  width: 48%;
+  float: left;
+}
+body .card .photo img {
+  height: 540px;
+  width: 300px;
+}
+body .card .description {
+  padding: 30px;
+  float: left;
+  width: 40%;
+  height: 15%;
+}
+body .card .description h1 {
+  color: #515151;
+  margin: 0;
+  text-transform: uppercase;
+  font-weight: 500;
+}
+body .card .description h2 {
+  margin: 10px;
+  color: #727272;
+  text-transform: uppercase;
+  font-weight: 500;
+  font-size: 12px;
+}
+body .card .description p {
+  font-size: 12px;
+  line-height: 20px;
+  color: #727272;
+  padding: 70px 0;
+  margin: 10px;
+  text-align: left;
+}
+        </style>
+        
+    </head>
+    <body>
+        <div class="card">
+            <div class="photo">
+              <img src="https://im.berrybenka.com/assets/upload/product/zoom/284419_yunna-janice-tote-shoulder-bag-beige_white_R5XM1.jpg">
+            </div>
+            <div class="description">
+              <h1>Totebag</h1>
+              <h2>Rp. 30,000</h2>
+              <p>Totebag putih dengan bahan kulit sisntetis yang membuat tampilan ada berkesan dan cocok digunakan untuk berbagai kalangan serta waktu kapanpun</p>
+
+            </div>
+        </div>
+
+            
+          </div>
+    </body>
+</html>
