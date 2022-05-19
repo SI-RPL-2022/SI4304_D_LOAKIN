2fc88c4dbdde6e4d5df776f98597a0c4c585463e
tiararll <tiaraaurellia02@gmail.com>
2022-05-19 10:19:27 +0700

    input berat done

--- resources/views/Driver/Page/Berat/InputBerat.blade.php
+++ resources/views/Driver/Page/Berat/InputBerat.blade.php
@@ -7,6 +7,11 @@
 
     <div class="container mt-4" style="border-style:solid; border-width:1px; border-color: #F58814;border-radius: 10px; padding:10px;">
         <div style="margin-left: 20px;margin-right:20px">
+            <div class="d-flex align-items-center" style="float:right">
+                <a class="pl-3 mr-4">
+                    <p class="m-0"><b>Kode Driver : {{session()->get('driver')->kodedriver}}</b></p>
+                </a>
+            </div>
             <table class="table-responsive" cellpadding="4">
                 <thead>
                     <tr style="color: #D77409">
@@ -19,47 +24,6 @@
                         <td></td>
                         <td><b>Jung Jaehyun</b></td>
                         <td rowspan="2">Jl. Telekomunikasi No. 1, Terusan Buahbatu - Bojongsoang, Sukapura, Kec. Dayeuhkolot, Kabupaten Bandung, Jawa Barat 40257</td>
-                        <td><b><a href="" style="color: #D77409;text-decoration:none" data-bs-toggle="modal" data-bs-target="#exampleModal">UBAH</a></b></td>
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered">
-                                <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel" style="color:#D77409">Alamat Baru</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <form>
-                                    <div class="modal-body">
-                                        <div class="row g-2 mb-3">
-                                            <div class="col-md">
-                                                <div class="form-floating">
-                                                    <input type="text" class="form-control" id="nama" placeholder="Nama">
-                                                    <label for="nama">Nama Lengkap</label>
-                                                </div>
-                                            </div>
-                                            <div class="col-md">
-                                                <div class="form-floating">
-                                                    <input type="number" class="form-control" id="nohp" placeholder="021472384">
-                                                    <label for="nohp">Nomor Telepon</label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-floating mb-3">
-                                            <textarea class="form-control" placeholder="Nama Jalan" id="floatingTextarea"></textarea>
-                                            <label for="floatingTextarea">Nama Jalan, Gedung, No. Rumah</label>
-                                        </div>
-                                        <div>
-                                            <img src="https://developers.google.com/maps/images/landing/hero_maps_static_api.png" class="rounded mx-auto d-block" style="width:25rem">
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer d-flex justify-content-center">
-                                        <button type="button" class="btn" data-bs-dismiss="modal" style="background-color:#FBCE9D;width:8rem">Close</button>
-                                        <button type="button" class="btn" style="background-color:#F58814;color:white;width:8rem">Save changes</button>
-                                    </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
                     </tr>
                     <tr>
                         <td></td>
@@ -76,21 +40,6 @@
                 <h5 style="color: #D77409; margin-left:20px"><b>Item Details</b></h5>
                 <div style="margin-left: 50px;margin-right:40px">
                     <div class="mb-3">
-                        <label for="weightm" class="form-label"><b>Weight Measure</b></label>
-                        <div class="form-check">
-                            <input class="form-check-input"  type="radio" name="dibawah" id="dibawah" >
-                            <label class="form-check-label" for="dibawah">
-                                Dibawah 10kg
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="diatas" id="diatas">
-                            <label class="form-check-label" for="diatas">
-                                Diatas 10kg
-                            </label>
-                        </div>
-                    </div>
-                    <div class="mb-3">
                         <label for="desc" class="form-label"><b>Item Description</b> (Ex : TV, paper,etc)</label>
                         <input type="text" class="form-control" id="desc" style="border-width:0 0 1px 0;box-shadow:none" placeholder="Kardus">
                     </div>
@@ -100,20 +49,11 @@
                     </div>
                     <div>
                         <label for="pay" class="form-label"><b>Payment</b></label>
+                        <p>Cash</p>
                     </div>
                     <div class="mb-3">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="cash" value="cash">
-                            <label class="form-check-label" for="cash">Cash</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="dana" value="dana">
-                            <label class="form-check-label" for="dana">Dana</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="ovo" value="ovo">
-                            <label class="form-check-label" for="ovo">OVO</label>
-                        </div>
+                        <label for="total" class="form-label"><b>Payment</b></label>
+                        <p>3000</p> <!-- weight * 3000 -->
                     </div>
                 </div>
             </div>
