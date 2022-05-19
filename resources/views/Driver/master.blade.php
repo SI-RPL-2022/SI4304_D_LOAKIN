26e6e858c5c3703cedcdb1079c756419438e6598
tiararll <tiaraaurellia02@gmail.com>
2022-05-19 10:07:25 +0700

    ubah pickup

--- resources/views/Driver/master.blade.php
+++ resources/views/Driver/master.blade.php
@@ -19,6 +19,10 @@
     <!-- Layout styles -->
     <link rel="stylesheet" href="{{asset('assetsdriver/css/demo_2/style.css')}}" />
     <!-- End layout styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <link rel="icon" type="image/png" href="{{asset('logoatas.png')}}">
   </head>
   <body>
@@ -60,5 +64,6 @@
     <!-- endinject -->
     <!-- Custom js for this page -->
     <script src="{{asset('assetsdriver/js/dashboard.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
