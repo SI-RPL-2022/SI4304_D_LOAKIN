a573967de73456ce9e43b74e741f591de4010fa3
amirajd <amirajauhara@gmail.com>
2022-05-23 07:23:28 +0700

    halaman keranjang

--- resources/views/User/Shop/Detail.blade.php
+++ resources/views/User/Shop/Detail.blade.php
@@ -1,82 +1,65 @@
-<html>
-    <head>
-        <title>Detail Product</title>
-        <style>
-            @import url(https://fonts.googleapis.com/css?family=Raleway:400,300,500,700);
-* {
-  box-sizing: border-box;
-}
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-body {
-  background: #f5f4f3;
-  font-family: "Raleway";
-}
-body .card {
-  width: 750px;
-  height: 600px;
-  position: absolute;
-  background: white;
-  margin: 0 auto;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-  transition: all 0.3s;
-  border-radius: 10px;
-}
-body .card .photo {
-  padding: 30px;
-  width: 48%;
-  float: left;
-}
-body .card .photo img {
-  height: 540px;
-  width: 300px;
-}
-body .card .description {
-  padding: 30px;
-  float: left;
-  width: 40%;
-  height: 15%;
-}
-body .card .description h1 {
-  color: #515151;
-  margin: 0;
-  text-transform: uppercase;
-  font-weight: 500;
-}
-body .card .description h2 {
-  margin: 10px;
-  color: #727272;
-  text-transform: uppercase;
-  font-weight: 500;
-  font-size: 12px;
-}
-body .card .description p {
-  font-size: 12px;
-  line-height: 20px;
-  color: #727272;
-  padding: 70px 0;
-  margin: 10px;
-  text-align: left;
-}
-        </style>
-        
-    </head>
-    <body>
-        <div class="card">
-            <div class="photo">
-              <img src="https://im.berrybenka.com/assets/upload/product/zoom/284419_yunna-janice-tote-shoulder-bag-beige_white_R5XM1.jpg">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<div class="container my-4">
+    <h2 class="text-center fw-bold fst-italic" style="color: rgb(239,136,48)">Detail Produk</h2>
+
+    <div class="card border-4 mb-3 mt-3 w-100 mx-auto rounded" style="border-color:rgb(239,136,48) ">
+        <div class="row p-5 pt-3">
+            <div class="col-5 mt-4">
+                <img src="img.png" alt="" class="w-100">
             </div>
-            <div class="description">
-              <h1>Totebag</h1>
-              <h2>Rp. 30,000</h2>
-              <p>Totebag putih dengan bahan kulit sisntetis yang membuat tampilan ada berkesan dan cocok digunakan untuk berbagai kalangan serta waktu kapanpun</p>
+            <div class="col mt-4">
+                    <h5 class="fw-bold" style="color: rgb(239,136,48)"> Totebag</h5>
+                    <p class="fw-bold text-danger">Rp. 30,000</p>
 
+                    <p class="mt-5">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores debitis dolores ducimus explicabo magnam, suscipit voluptatum? Architecto assumenda deserunt eaque earum eum fuga, numquam officiis quibusdam, reprehenderit unde vero.
+                    </p>
+
+                <h5 class="fw-bold mt-5" style="color: rgb(239,136,48)"> Spesifikasi Produk</h5>
+                <div class="mt-2">
+                   <p>Bahan Tas : Kulit Tas
+                    <br>Ukuran Tas: 190cm x 200cm
+                       <br> Warna Tas: Putih Tulang
+                   <br>Warna Tali Tas : Coklat Tua
+                   <br>Pemakaian: 2 Bulan
+                    <br>Cacat: Terdapat Noda Garis Hitam Di bawah
+                    <br>Tambahan: Dusk Bag dan Kotak
+                    <br>Dikirim Dari : Jakarta</p>
+
+                    <p class="mt-2">Dijami Original!!
+                    <br>Cocok Digunakan segala umru
+                    <br>Warna Sesuai Gambar</p>
+                </div>
             </div>
         </div>
 
-            
-          </div>
-    </body>
+    </div>
+    <div class="d-flex justify-content-end">
+        <button style="background-color: rgb(239,136,48)" class="btn btn-md text-white w-25 align-right">Masukan keranjang</button>
+    </div>
+</div>
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+-->
+</body>
 </html>
