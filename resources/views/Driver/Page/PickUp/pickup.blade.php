23e05ba5cc44e86308eca1f93bdc23689d3afd1a
rymarshandasw <rymarshandas@gmail.com>
2022-05-23 18:50:53 +0700

    list pickup 80%

--- resources/views/Driver/Page/PickUp/pickup.blade.php
+++ resources/views/Driver/Page/PickUp/pickup.blade.php
@@ -1,1 +1,50 @@
-<h1>ini list pickup</h1>
+@extends('Driver.Master')
+
+@section('content')
+            <div class="page-header flex-wrap">
+              <div class="header-left">
+                
+              </div>
+              <div class="header-right d-flex flex-wrap mt-md-2 mt-lg-0">
+                <div class="d-flex align-items-center">
+                  <a href="#">
+                    <p class="m-0 pr-3">Dashboard</p>
+                  </a>
+                  <a class="pl-3 mr-4" href="#">
+                    <p class="m-0">{{session()->get('driver')->kodedriver}}</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+                {{-- Judul --}}
+    <div class="container d-flex justify-content-center mt-3">
+        <h2 style="font-family: 'Nunito'; font-style: italic; font-weight:900; color:#D77409; text-align:center; font-size:38px; line-height: 52px; text-shadow: 3px 2px #FDE0C3;">Pick Up Loak.in Aja</h2>
+    </div>
+
+    <div class="comtainer mt-5" style=" font-family: 'Segoe UI', Arial, sans-serif;">
+        <table class="table table-responsive table-bordered">
+            <thead style="font-weight: 600; background-color:#F58814;color:white;">
+                <tr>
+                    <th scope="col"><center><b>No</b></center></th>
+                    <th scope="col" style="width: 50%"><center><b>Customer Name</b></center></th>
+                    <th scope="col"><center></center></th>
+                </tr>
+            </thead>
+    
+            <tbody>
+                <tr>  
+                    <th scope="row" style="line-height: 8rem; text-align: center">1</th>
+                    <td style="line-height: 8rem;text-align: center">Jung Jaehyun</td>
+                    <td style="line-height: 8rem;text-align: center"><center>
+                        <a class="btn btn-outline-light" href='#' style="font-weight: 600; background-color:#F58814;color:white;border-radius:7px 7px 7px 7px;width:15rem;">PICK UP</button>
+                        <a class="btn btn-danger" href='#' onclick="return confirm('Are you sure?')" style="font-weight: 600;color:white;border-radius:7px 7px 7px 7px;width:15rem;">DELETE</a>
+                    </center></td>
+                </tr>
+            </tbody>
+
+        </table>
+    </div>
+
+
+@endsection
