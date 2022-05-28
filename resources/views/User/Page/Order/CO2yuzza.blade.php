182fca36ffd76d074fbb81fe1d625b8bf042ac0f
Yumna Zahran Ramadhan <ezzayumna@student.telkomuniversity.ac.id>
2022-05-28 13:54:31 +0700

    push 6

---
+++ resources/views/User/Page/Order/CO2yuzza.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<title> Checkout Loakin </title>
+</head>
+
+
+<body>
+    <div class="container d-flex justify-content-center mt-3">
+        <h1 style="font-family: 'Montserrat';color:#D77409"><b>Checkout Barang</b></h4>
+    </div>
+
+    <div class="container dflex- justify-content">
+        <h3 style="font-family: 'Montserrat';color:#D77409"><b>Checkout Number ####</b></h3>
+    </div>
+
+    <div class="container mt-4 my-5" style="border-style:solid; border-width:1px; border-color: #F58814;border-radius: 10px; padding:10px;">
+        <div style="margin-left: 20px;margin-right:20px">
+                <div style="margin-left: 50px;margin-right:40px">
+                    <div class="mb-3">
+                        <label for="weightm" class="form-label"><b>Order</b></label>
+                        <h5 style="color: red; ">Rp.150.000</h5>
+                    </div>
+                    <div class="mb-3">
+                        <label for="desc" class="form-label"><b>Delivery</b></label>
+                        <h5 style="color: red; ">Rp.40.000</h5>
+                    </div>
+                    <div class="mb-3">
+                        <label for="weight" class="form-label"><b>Summary</b></label>
+                        <h5 style="color: red; ">Rp.190.000</h5>                    </div>
+                    </div>
+            </div>
+    </div>
+
+    <div class="container dflex- justify-content-center mt-3">
+        <h3 style="font-family: 'Montserrat';color:#D77409"><b>Payment Checkout</b></h3>
+    </div>  
+
+    <div class="container mt-4" style="border-width:1px; border-color: #F58814;border-radius: 10px; padding:10px;">
+        <div style="margin-left: 20px;margin-right:20px">
+            <table class="table-responsive" cellpadding="">
+                <thead>
+                    <tr style="color: #D77409">
+                        <td scope="col"><h5><i class="bi bi-geo-alt-fill" ></i></h5></td>
+                    </tr>
+                </thead>
+
+                <tbody>   
+                    <tr>
+                        <td>
+                            <img src="https://1.bp.blogspot.com/-YZb4sqBo4wM/XqcBd4qYwVI/AAAAAAAAAQg/8F60s1NOLJcNGFGRryqYQw_wvxg6d6VtQCLcBGAsYHQ/s1600/OVO%2B%255Bwww.blogovector.com%255D.png" width="300" height="200"></img>
+                        </td>
+                        <td>
+                            <img src="https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/ca5d12864c12916c05640b36e47ac5c9.png" width="300" height="200"></img>
+                        </td>
+                        <td>
+                            <img src="https://1.bp.blogspot.com/-fvIxf9jrXBQ/Xytve_Yq_uI/AAAAAAAAClw/31J-RhOBi5Qc5oCmP8-UUhgZE-oSoeJWwCLcBGAsYHQ/s1600/Logo%2BDana%2BBackground%2BBiru.jpg" width="300" height="200"></img>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <center>
+                            <input type="radio" name="ovo" value="ovo"> 
+                            </center>
+                        </td>
+                        <td>
+                            <center>
+                            <input type="radio" name="shopee" value="shopee"> 
+                            </center>
+                        </td>
+                        <td>
+                            <center>
+                            <input type="radio" name="dana" value="dana"> 
+                            </center>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>    
+
+        </div>
+    </div>
+    
+    <div class="d-flex" style="margin-top: 30px;">
+        <button class="btn ms-auto p-2 bd-highlight" type="submit" style="background-color:#F58814;color:white;border-radius:7px 7px 7px 7px;width:15rem"> CHECKOUT </button>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
