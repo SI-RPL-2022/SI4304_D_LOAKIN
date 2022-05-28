99d7dcd7f9512543e8aa1795d8bd4215c59a7c8f
Yumna Zahran Ramadhan <ezzayumna@student.telkomuniversity.ac.id>
2022-05-28 12:04:11 +0700

    1202194191

---
+++ resources/views/User/Page/Order/CO1yuzza.blade.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<title> Checkout Loakin </title>
+</head>
+
+
+<body>
+    <div class="container d-flex justify-content-center mt-3">
+        <h1 style="font-family: 'Montserrat';color:#D77409"><b>Checkout Barang</b></h4>
+    </div>
+
+    <div class="container mt-4" style="border-style:solid; border-width:1px; border-color: #F58814;border-radius: 10px; padding:10px;">
+        <div style="margin-left: 20px;margin-right:20px">
+            <table class="table-responsive" cellpadding="4">
+                <thead>
+                    <tr style="color: #D77409">
+                        <td scope="col"><h5><i class="bi bi-geo-alt-fill" ></i></h5></td>
+                        <td scope="col" colspan="2"><h5><b>Alamat</b></h5></td>
+                    </tr>
+                </thead>
+                <tbody>   
+                    <tr>
+                        <td></td>
+                        <td><b>Olivia Hye</b></td>
+                        <td> 12 Seolleung-ro 103-gil, Yeoksam 1(il)-dong Gangnam-gu, Seoul, South Korea </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><b>(+62)81267937770</b></td>
+                    </tr>
+                </tbody>
+            </table>    
+        </div>
+    </div>
+
+    <div class="container mt-4" style="border-style:solid; border-width:1px; border-color: #F58814;border-radius: 10px; padding:10px;">
+        <div style="margin-left: 20px;margin-right:20px">
+            <table class="table-responsive" cellpadding="">
+                <thead>
+                    <tr style="color: #D77409">
+                        <td scope="col"><h5><i class="bi bi-geo-alt-fill" ></i></h5></td>
+                        <td scope="col" colspan="2"><h5><b>Delivery Method</b></h5></td>
+                    </tr>
+                </thead>
+
+                <tbody>   
+                    <tr>
+                        <td>
+                            <img src="https://th.bing.com/th/id/OIP.x4KGY0uFH5CmdkwrHcVfEAHaE8?pid=ImgDet&rs=1"></img>
+                        </td>
+                        <td>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <input type="radio" name="JNT" value="5 - 7 Days"> <h5> 5 - 7 days </h5>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>    
+
+        </div>
+    </div>
+
+    <div class="container mt-4" style="border-style:solid; border-width:1px; border-color: #F58814;border-radius: 10px; padding:10px;">
+        <div style="margin-left: 20px;margin-right:20px">
+                <h5 style="color: #D77409; margin-left:20px"><b> Checkout Details </b></h5>
+                <div style="margin-left: 50px;margin-right:40px">
+                    <div class="mb-3">
+                        <label for="weightm" class="form-label"><b>Order</b></label>
+                        <h5 style="color: red; ">Rp.150.000</h5>
+                    </div>
+                    <div class="mb-3">
+                        <label for="desc" class="form-label"><b>Delivery</b></label>
+                        <h5 style="color: red; ">Rp.40.000</h5>
+                    </div>
+                    <div class="mb-3">
+                        <label for="weight" class="form-label"><b>Summary</b></label>
+                        <h5 style="color: red; ">Rp.190.000</h5>                    </div>
+                    </div>
+                    <div class="d-flex" style="margin-top: 30px;">
+                        <button class="btn ms-auto p-2 bd-highlight" type="submit" style="background-color:#F58814;color:white;border-radius:7px 7px 7px 7px;width:15rem"> CHECKOUT </button>
+                    </div>
+            </div>
+    </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
