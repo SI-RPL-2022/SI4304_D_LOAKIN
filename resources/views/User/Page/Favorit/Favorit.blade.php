30a572f39b99f56cc03e83493b568f5a65a624f3
amirajd <amirajauhara@gmail.com>
2022-06-14 20:17:47 +0700

    halaman awal favorit

---
+++ resources/views/User/Page/Favorit/Favorit.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Favorit Saya</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+  
+    <!-- end header section -->
+    <!-- slider section -->
+    <div class="container">
+
+<h2 class="text-center">Favorit Saya</h2>
+
+    <table class="table table-bordered"  style="width:80%">
+      <thead>
+        <tr>
+          <th scope="col" style="text-align: center">No</th>
+          <th scope="col" style="text-align: center">Gambar</th>
+          <th scope="col" style="text-align: center">Nama Produk</th>
+          <th scope="col" style="text-align: center">Harga</th>
+          <th scope="col" style="text-align: center">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td><img src="images/foto1.jpg" style="height:100px"></td>
+          <td style="text-align: center">Tas</td>
+          <td style="text-align: center">Rp 30.000</td>
+          <td style="text-align: center"><a class="btn btn-warning">Detail</a>
+            <a class="btn btn-danger">Hapus</a></td>
+        
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td><img src="images/foto2.jpg" style="height:100px"></td>
+          <td style="text-align: center">Tas Thornton</td>
+          <td style="text-align: center">Rp 40.000</td>
+          <td style="text-align: center"><a class="btn btn-warning">Detail</a> 
+            <a class="btn btn-danger">Hapus</a>
+            </td>
+        </tr>
+        
+      </tbody>
+    </table>
+
+  </div>
+
+  <footer class="fixed-bottom">
+  <!-- footer section -->
+  
+  <section class="container-fluid footer_section">
+    <div class="container">
+    <h5 style="text-color:white">Tentang Kami :</h5>
+    <br>
+    <p>Alamat : Jl. Telekomunikasi Jl. Terusan Buah Batu, Sukapura, <br><p>Kec. Dayeuhkolot,
+      kota Bandung, Jawa Barat 40257</p>
+    </p>
+  </div>
+  </section>
+  <!-- footer section -->
+</footer>
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+
+ 
+  <!-- google map js -->
+  
+  </script>
+  <!-- end google map js -->
+</body>
+
+</html>
