b40f253897332264907c01504f806d051ac06324
rymarshandasw <rymarshandas@gmail.com>
2022-06-18 16:36:02 +0700

    revisi almost done

--- resources/views/User/Page/Pembayaran/pembayaran.blade.php
+++ resources/views/User/Page/Pembayaran/pembayaran.blade.php
@@ -8,7 +8,7 @@
             <form class="form form-horizontal" action="{{ route('bayar.updateimg', $bayar->id) }}" method="POST" enctype="multipart/form-data">
                 @csrf
             <center><h4>No Order : {{$bayar->no_order}}</h4></center>
-            <h6 class="mt-2 pb-4"><b>Upload proof of payment</b></h6>
+            <h6 class="mt-2 pb-4"><b>Upload Proof of Payment</b></h6>
             <img src="https://drive.google.com/uc?export=view&id=10Yp9_5oSnFaXsitEkZXCRaT6d64l28J2" width="90" height="24" alt="">
             <p class="mt-3"><b>Mandiri Bank</b></p>
             <br>
@@ -16,7 +16,7 @@
             <p style="color: red">896 0819 1017 5601</p>
             <br>
             <p style="font-size:14px">Payment Time is only 10 minutes</p>
-            <p style="color: blue; font-size:14px;">The account number owner's name is Haikal D</p>
+            <p style="color: blue; font-size:14px;">The account number owner's name is Haikal Putra</p>
             <div class="mb-3">
                 <input type="file" class="form-control" value="{{old('foto')}}" name="foto" >
             </div>
