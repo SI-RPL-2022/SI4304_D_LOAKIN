bdfabaa2a2016de6c49cf81965a67570c9ee7fe9
Yumna Zahran Ramadhan <ezzayumna@student.telkomuniversity.ac.id>
2022-06-19 11:21:15 +0700

    co3barang

---
+++ resources/views/User/Page/Order/CO3yuzza.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<title> Checkout Loakin </title>
+</head>
+
+
+<body>
+    <div class="container d-flex justify-content-center mt-3">
+        <h1 style="font-family: 'Montserrat';color:#D77409"><b>Checkout Barang</b></h4>
+    </div>
+
+    <div class="container dflex- justify-content">
+        <h3 style="font-family: 'Montserrat';color:#D77409"><b>Checkout Number ####</b></h3>
+    </div>
+
+    <div class="container mt-4 my-5" style="border-style:solid; border-width:1px; border-color: #F58814;border-radius: 10px; padding:10px;">
+        <div style="margin-left: 20px;margin-right:20px">
+                <div style="margin-left: 50px;margin-right:40px">
+                    <div class="mb-3">
+                        <label for="weightm" class="form-label"><b>Order</b></label>
+                        <h5 style="color: red; ">Rp.150.000</h5>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="weightm" class="form-label"><b>Payment by</b></label>
+                        <h5 style="color: red; "> DANA </h5>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="weightm" class="form-label"><b>Upload Payment Proof</b></label>
+                    </div>
+
+                    <div>
+                        <button class="btn ms-auto p-2 bd-highlight" type="submit" style="background-color:#F58814;color:white;border-radius:7px 7px 7px 7px;width:15rem"> Upload Here </button>
+                    </div>    
+
+                    <div>
+                    <h9 style="color: #B3B3B3; ">Maks 5 mb</h9>
+                    </div>
+                </div>
+    </div>
+
+    <div class="container mt-4" style="border-width:1px; border-color: #F58814;border-radius: 10px; padding:10px;">
+        <div style="margin-left: 20px;margin-right:20px">
+            <table class="table-responsive" cellpadding="">
+                <thead>
+                    <tr style="color: #D77409">
+                        <td scope="col"><h5><i class="bi bi-geo-alt-fill" ></i></h5></td>
+                    </tr>
+                </thead>
+            </table>    
+
+        </div>
+    </div>
+    
+    <div class="d-flex" style="margin-top: 30px;">
+        <button class="btn ms-auto p-2 bd-highlight" type="submit" style="background-color:#F58814;color:white;border-radius:7px 7px 7px 7px;width:15rem"> CHECKOUT </button>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
