10f929ac663063935cc6d7e918b16d7c31f42f85
rymarshandasw <rymarshandas@gmail.com>
2022-06-19 10:01:21 +0700

    revisi ubah alamat

--- resources/views/User/Page/Order/ubahalamat.blade.php
+++ resources/views/User/Page/Order/ubahalamat.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 
-    <div class="container d-flex justify-content-center mt-3">
-        <h1 style="font-family: 'Montserrat';color:#D77409"><b>Ubah Alamat</b></h4>
-    </div>
+<div class="container d-flex justify-content-center mt-3">
+    <h2 style="font-family: 'Nunito'; font-style: italic; font-weight:900; color:#D77409; text-align:center; font-size:38px; line-height: 52px; text-shadow: 3px 2px #FDE0C3;">Change Address </h2>
+</div>
     @if($alamat->isEmpty())
     <center>
-        <h5>BELOM ADA ALAMAT TAMBAHAN NIH...</h5>
+        <h5>No other address</h5>
         <div class="col-md-3">
             <a href="{{route('account.alamat', $account->id)}}" class="btn btn-outline-light " style="background-color:#F58814;font-color:white; width:100%" type="submit" name="submit">
-            <i class="fa fa-plus" aria-hidden="true" style="margin-right: 10px"></i> Tambah Alamat</a>
+            <i class="fa fa-plus" aria-hidden="true" style="margin-right: 10px"></i> Add Address</a>
         </div>
     </center>
     @else
@@ -23,7 +23,7 @@
                     <input class="form-check-input " type="radio" id="alamat" name="alamat" value="{{$al->alamat}}">
                     <label class="form-check-label" for="alamat1">
                         <div>
-                            <table class="table-responsive" cellpadding="5">
+                            <table class="table-responsive" cellpadding="5" style="font-weight: 500;">
                                 <tbody>   
                                     <tr>
                                         <td></td>
