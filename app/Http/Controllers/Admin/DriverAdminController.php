7a437e67b01a9c9aa5ab658d01ffa1eb567547bc
tiararahmania05 <tiararahmania05@gmail.com>
2022-06-20 12:15:49 +0700

    Final Regist Driver

--- app/Http/Controllers/Admin/DriverAdminController.php
+++ app/Http/Controllers/Admin/DriverAdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Str;
 use App\Models\Driver;
 
 use Session;
@@ -27,6 +28,52 @@ class DriverAdminController extends Controller
     {
         $driver = Driver::find($id);
         return view('Admin.Page.DataDriver.Show', compact('driver'));
+    }
+
+    public function create_view()
+    {
+
+        return view('Admin.Page.DataDriver.Create');
+    }
+
+    public function create_process(Request $request)
+    {
+        $request->validate([
+            'nama_driver'               => 'required',
+            'username'               => 'required',
+            'password'              => 'required',
+            'telepon'             => 'required',
+            'alamat'             => 'required',
+        ]);
+
+        $request->validate([
+            'foto' => 'mimes:jpg,jpeg,png',
+        ]);
+
+        $driver = new Driver;
+        $numberorder = Str::random(5);
+
+        
+            if ($files = $request->file('foto')) {
+                $destinationPath = 'fotodriver/';
+                $file = $request->file('foto');
+                // upload path  
+    
+                $profileImage = basename($request->file('foto')->getClientOriginalName(), '.' . $request->file('foto')->getClientOriginalExtension()) . "." .
+                    $files->getClientOriginalExtension();
+                $pathImg = $file->storeAs('', $profileImage);
+                $files->move($destinationPath, $profileImage);
+                $driver->foto = $pathImg;
+            }
+            $driver->name         = $request->nama_driver;
+            $driver->username         = $request->username;
+            $driver->password               = $request->password;
+            $driver->nomortelepon       = $request->telepon;
+            $driver->alamat       = $request->alamat;
+            $driver->kodedriver       = $numberorder;
+            $driver->save();
+
+        return redirect(route('driverdata.index'))->with(['success' => 'Registrasi Driver Berhasil']);
     }
 
 }
