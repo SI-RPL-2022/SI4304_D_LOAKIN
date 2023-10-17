94d63d667d2a318d196200d5539db63502707fef
Tiara Rahmania <tiararahmania05@gmail.com>
2023-10-17 11:06:35 +0700

    versi 2

--- app/Http/Controllers/User/HomeUserController.php
+++ app/Http/Controllers/User/HomeUserController.php
@@ -7,10 +7,6 @@ use Illuminate\Http\Request;
 
 class HomeUserController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware(['auth','verified']);
-    }
     
     public function index()
     {
